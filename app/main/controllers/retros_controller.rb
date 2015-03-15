42d03cf101b5759613ad685aef4d11f07bf583c7
Shelby Doolittle <shelby@shelbyd.com>
2015-03-15 16:12:39 -0700

    group retro items by category

--- app/main/controllers/retros_controller.rb
+++ app/main/controllers/retros_controller.rb
@@ -1,13 +1,14 @@
 class RetrosController < Volt::ModelController
   def show
     self.model = page
+    page._new_item._category = page._new_item._category.or('happy')
     store._retros.find({parameter_name: params._name }).then do |results|
       self.model = results[0]
     end
   end
 
   def add_item
-    model._items << { text: page._new_item }
-    page._new_item = ''
+    _items << page._new_item.to_h
+    page._new_item = Volt::Model.new category: 'happy'
   end
 end
